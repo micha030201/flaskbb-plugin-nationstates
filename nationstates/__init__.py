5672e9b3507631a77b59c2b123f6b8518e870aad
micha <micha030201@gmail.com>
2018-09-08 17:37:28 +0300

    rmb quotes in forum messages

--- nationstates/__init__.py
+++ nationstates/__init__.py
@@ -9,12 +9,17 @@
     :license: BSD License, see LICENSE for more details.
 """
 import os
+import re
 import logging
 
+import bbcode
+from pluggy import HookimplMarker
 import requests
+import mistune
 from wtforms import StringField
 from wtforms.validators import DataRequired, regexp, ValidationError
-from flask import render_template_string
+from flask import render_template_string, Markup, Blueprint
+from flaskbb.utils.helpers import render_template
 
 from .models import User
 
@@ -22,12 +27,23 @@ from .models import User
 __version__ = "0.0.1"
 
 
+hookimpl = HookimplMarker("flaskbb")
 logger = logging.getLogger(__name__)
 
 
 # connect the hooks
 def flaskbb_load_migrations():
     return os.path.join(os.path.dirname(__file__), "migrations")
+
+
+bp = Blueprint("nationstates", __name__, template_folder="templates")
+
+
+def flaskbb_load_blueprints(app):
+    app.register_blueprint(
+        bp,
+        url_prefix="/nationstates"
+    )
 
 
 # Monkeypatch registration
@@ -132,6 +148,99 @@ def flaskbb_tpl_post_author_info_after(user, post):
     if user.nation is not None:
         return ('<a href="https://nationstates.net/{0}">{0}</a>'
                 .format(user.nation))
+
+
+# Post syntax extensions:
+
+class BlockGrammar(mistune.BlockGrammar):
+    nsquote = re.compile(
+        '\[quote=([A-Za-z0-9_-]+);([0-9]+)\](.+?)\[/quote\]',
+        flags=re.DOTALL
+    )
+
+
+class BlockLexer(mistune.BlockLexer):
+    grammar_class = BlockGrammar
+    default_rules = ['nsquote'] + mistune.BlockLexer.default_rules
+
+    def parse_nsquote(self, m):
+        self.tokens.append({
+            'type': 'nsquote',
+            'author': m.group(1),
+            'post_id': m.group(2),
+            'text': m.group(3),
+        })
+
+
+class Markdown(mistune.Markdown):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.block = BlockLexer(BlockGrammar())
+
+    def output_nsquote(self):
+        author = self.token['author']
+        post_id = self.token['post_id']
+        text = self.token['text']
+        return self.renderer.nsquote(author=author, post_id=post_id, text=text)
+
+
+bbcode_parser = bbcode.Parser(
+    newline='<br />',
+    install_defaults=False,
+    escape_html=True,
+    replace_links=True,
+    url_template='<a target="_blank" href="{href}">{text}</a>',
+    replace_cosmetic=True,
+)
+bbcode_parser.add_simple_formatter('b', '<strong>%(value)s</strong>')
+bbcode_parser.add_simple_formatter('i', '<i>%(value)s</i>')
+bbcode_parser.add_simple_formatter('u', '<u>%(value)s</u>')
+
+
+def render_nation_region(tag_name, value, options, parent, context):
+    if value[0].isupper():
+        name = value.replace('_', ' ')
+    else:
+        name = value.replace('_', ' ').title()
+    return (
+        '<a target="_blank" href="https://www.nationstates.net/'
+        '{type}={name_url}">{name}</a>'
+        .format(type=tag_name, name=name,
+                name_url=name.replace(' ', '_').lower())
+    )
+
+
+bbcode_parser.add_formatter('nation', render_nation_region)
+bbcode_parser.add_formatter('region', render_nation_region)
+
+
+class Renderer:
+    def nsquote(self, author, post_id, text):
+        return render_template(
+            'rmb_quote.html',
+            author=author,
+            post_id=post_id,
+            text=Markup(bbcode_parser.format(text))
+        )
+
+
+def flaskbb_load_post_markdown_class():
+    return Renderer
+
+
+def make_renderer(classes):
+    RenderCls = type('FlaskBBRenderer', tuple(classes), {})
+
+    markup = Markdown(renderer=RenderCls(escape=True, hard_wrap=True))
+    return lambda text: Markup(markup.render(text))
+
+
+# Unfortunately, it's not possible to extend the mistune.BlockLexer
+# without re-running this hook
+@hookimpl(trylast=True)
+def flaskbb_jinja_directives(app):
+    render_classes = app.pluggy.hook.flaskbb_load_post_markdown_class(app=app)
+    app.jinja_env.filters['markup'] = make_renderer(render_classes)
 
 
 SETTINGS = {}
